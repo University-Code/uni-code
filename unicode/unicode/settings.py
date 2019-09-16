44a7e8d9cfa30275c336dde5e3596e7d2d610cc6
Kailyn Williams <kailynwilliams@kailyns-air.uscupstate.edu>
2019-09-16 15:35:22 -0400

    added template for project

--- unicode/unicode/settings.py
+++ unicode/unicode/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'problems.apps.ProblemsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
