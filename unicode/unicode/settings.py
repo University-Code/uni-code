3daa66d1400f104548ed5dd901c55e2b37da7e83
JP <johnpaulkiser@gmail.com>
2019-09-22 10:54:07 -0400

    Create django app users

--- unicode/unicode/settings.py
+++ unicode/unicode/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'problems.apps.ProblemsConfig',
-
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
