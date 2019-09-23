8d1c189aa12f2ae300aa66d807b628747e1bc7be
Kailyn Williams <kailynwilliams@kailyns-air.uscupstate.edu>
2019-09-23 13:31:28 -0400

    Created code editor with Ace Editor API | Working on language switch and color mode function

--- unicode/problems/static/problems/myscripts.js
+++ unicode/problems/static/problems/myscripts.js
@@ -1,11 +1,72 @@
 
-$(".account").click(()=>{
-    $(".fa-chevron-down").toggleClass("rotate180")
-   // $("#navigation").stop().slideToggle(500)
+$(document).ready(()=>{
+    // var code= $(".codemirror-textarea")[0];
+    // options= {
+    //     lineNumbers: true,
+    //     keyMap: "sublime",
+    //     lineNumbers: true,
+    //     mode: "javascript",
+    //    autoCloseBrackets: true,
+    //     matchBrackets: true,
+    //     showCursorWhenSelecting: true,
+    //     theme: "monokai",
+    //     tabSize: 2
+
+    // }
+    // var editor= CodeMirror.fromTextArea(code, options)
+
+    
+
+    //Initializes Editor
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+
+    //Changes Language based on selection
+    $('select').change(()=>{
+        option= languageSelection($('select').val());
+        editor.session.setMode("ace/mode/"+option);
+
+        console.log(option)
+    })
+        
+
+     //Rotates arrow on menu drop down
+    $(".account").click(()=>{
+        $(".fa-chevron-down").toggleClass("rotate180")
+    })
+
+    // Shows/Hides User account menu
+    $('.account').click(()=> {
+        $('.user-menu').toggle()
+    })
+
+
+    
+
+
+
 })
 
 
-$('.account').click(()=> {
-    $('.user-menu').toggle()
-})
+function languageSelection(option){
+    option= option.toLowerCase()
+    var select='javascript';
+
+    switch(option){
+        case "c++":
+            select= 'c_cpp'
+            break;
+        case "c#":
+            select="csharp"
+            break;
+        default:
+            select=option
+            break;
+    }
+
+    return select
+}
+   
+
 
