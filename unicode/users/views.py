2324103db99257fa3a162fa9e1ef8d723354c50e
JP <johnpaulkiser@gmail.com>
2019-09-26 22:25:19 -0400

    Add User creation functionality to /register
    Added new dependecy 'cripsy-forms' to requirements for simple form styling.

--- unicode/users/views.py
+++ unicode/users/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from . forms import UserRegistrationForm
+from django.contrib import messages
 
 def register(request):
-    registration_form = UserCreationForm()
+    if request.method == 'POST':
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            username = registration_form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            print("yo")
+            return redirect('problems-landing')
+            
+    else:
+        registration_form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form' : registration_form})
