18a2c33a8eff2665f005bb054ab1abb63542946b
John Paul Kiser <johnpaulkiser@gmail.com>
2019-10-03 00:12:31 -0400

    Remove sidebar from auth pages

--- unicode/unicode/urls.py
+++ unicode/unicode/urls.py
@@ -12,7 +12,7 @@ from problems.views import index
 admin.autodiscover()
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('admin/', admin.site.urls,),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
