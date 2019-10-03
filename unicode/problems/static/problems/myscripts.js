bba460724186f98de2679288ba0c2bd27270f6f9
Kailyn Williams <kailynwilliams@iphone.uscupstate.edu>
2019-10-03 13:05:50 -0400

    working on UI and UX of text editor (theme switch, full screen mode)

--- unicode/problems/static/problems/myscripts.js
+++ unicode/problems/static/problems/myscripts.js
@@ -1,33 +1,62 @@
+functionName= "playGround"
+
+//Boiler Plate Code for autoloading code
+boilierPlate={
+    //JavaScript
+    "javascript":`function ${functionName}(word){
+               \r}`,
+    
+    //Java
+    "java":`class Program{\r\tpublic static void main (String[]args){
+                    \r\t\tString ${functionName}(String word){
+                        \r\t\t\tSystem.out.println("Hello World!")
+                        \r\t\t}\r\t}\r}`,
+    
+    //Python
+    "python": `def ${functionName}(word):`,
+
+    //C#
+    "csharp": 
+    
+            `public class Program{\r\tpublic static void ${functionName}(){
+                \r\t\tConsole.WriteLine("Hello World!");
+                \r\t}
+          \r}`,
+
+     //C++       
+    "c_cpp": 
+            `#include <iostream>\nusing namespace std;\nint ${functionName}(){
+                \r\tcout << "Hello World" << endl;\r\treturn 0;
+            \r}`,
+    //Clojure
+    "clojure": "IDK"
+
+}
+
 
 $(document).ready(()=>{
-    // var code= $(".codemirror-textarea")[0];
-    // options= {
-    //     lineNumbers: true,
-    //     keyMap: "sublime",
-    //     lineNumbers: true,
-    //     mode: "javascript",
-    //    autoCloseBrackets: true,
-    //     matchBrackets: true,
-    //     showCursorWhenSelecting: true,
-    //     theme: "monokai",
-    //     tabSize: 2
-
-    // }
-    // var editor= CodeMirror.fromTextArea(code, options)
-
     
-
     //Initializes Editor
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/javascript");
+    editor.setValue(boilierPlate['javascript'],0);
 
     //Changes Language based on selection
     $('select').change(()=>{
         option= languageSelection($('select').val());
         editor.session.setMode("ace/mode/"+option);
+        console.log('yerr', option)
+        editor.setValue(boilierPlate[option],0);
+    
+    })
 
-        console.log(option)
+    //Updates Text editor code 
+    $("#editor").keyup(()=>{
+       language= languageSelection($('select').val())
+       boilierPlate[language]= editor.getValue()
+       console.log(boilierPlate[language])
+       //boilerPlate.getValue())
     })
         
 
@@ -40,11 +69,6 @@ $(document).ready(()=>{
     $('.account').click(()=> {
         $('.user-menu').toggle()
     })
-
-
-    
-
-
 
 })
 
