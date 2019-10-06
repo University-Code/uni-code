a9cb82c4ddde739821ad84582aa2c0f87e1748a8
JP <johnpaulkiser@gmail.com>
2019-10-05 23:08:16 -0400

    Create Problem & ProblemTestCase models

--- unicode/problems/models.py
+++ unicode/problems/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import  
 
 # Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    example_solution= models.CharField(max_length=1000)
+    user_submitted = models.ForeignKey('User')
+
+class ProblemTestCase(models.Model):
+    problem = models.ForeignKey('Problem', on_delete=models.CASCADE)
+    datatype = models.CharField(max_length=10)
+    test_input = models.CharField(max_length=1000)
+    test_output = models.CharField(max_length=1000)
+    
+
+    
+    
+
+  
