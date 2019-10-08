87998adafc2a2bdfec62e8cb23e9cf795d16679f
Kailyn Williams <kailynwilliams@kailyns-air.lan>
2019-10-07 22:25:09 -0400

    Switched Editor and Descripion and Added more UI to description

--- unicode/problems/static/problems/myscripts.js
+++ unicode/problems/static/problems/myscripts.js
@@ -1,37 +1,7 @@
-functionName= "playGround"
+functionName= "reverseString"
+boilierPlate= boilerPlateCode();
+difficultyColor();
 
-//Boiler Plate Code for autoloading code
-boilierPlate={
-    //JavaScript
-    "javascript":`function ${functionName}(word){
-               \r}`,
-    
-    //Java
-    "java":`class Program{\r\tpublic static void main (String[]args){
-                    \r\t\tString ${functionName}(String word){
-                        \r\t\t\tSystem.out.println("Hello World!")
-                        \r\t\t}\r\t}\r}`,
-    
-    //Python
-    "python": `def ${functionName}(word):`,
-
-    //C#
-    "csharp": 
-    
-            `public class Program{\r\tpublic static void ${functionName}(){
-                \r\t\tConsole.WriteLine("Hello World!");
-                \r\t}
-          \r}`,
-
-     //C++       
-    "c_cpp": 
-            `#include <iostream>\nusing namespace std;\nint ${functionName}(){
-                \r\tcout << "Hello World" << endl;\r\treturn 0;
-            \r}`,
-    //Clojure
-    "clojure": "IDK"
-
-}
 
 
 $(document).ready(()=>{
@@ -43,17 +13,26 @@ $(document).ready(()=>{
     editor.setValue(boilierPlate['javascript'],0);
 
     //Changes Language based on selection
-    $('select').change(()=>{
-        option= languageSelection($('select').val());
+    $('.select-language').change(()=>{
+        option= languageSelection($('.select-language').val());
         editor.session.setMode("ace/mode/"+option);
         console.log('yerr', option)
         editor.setValue(boilierPlate[option],0);
     
     })
 
+    $('.select-theme').change(()=>{
+        theme= $('.select-theme').val().toLowerCase()
+        console.log(theme)
+        editor.setTheme("ace/theme/"+theme)
+
+        //Close window
+        $(".close").trigger('click')
+    })
+
     //Updates Text editor code 
     $("#editor").keyup(()=>{
-       language= languageSelection($('select').val())
+       language= languageSelection($('.select-language').val())
        boilierPlate[language]= editor.getValue()
        console.log(boilierPlate[language])
        //boilerPlate.getValue())
@@ -91,6 +70,65 @@ function languageSelection(option){
 
     return select
 }
+
+function difficultyColor(){
+   difficulty= $('.difficulty').html().toLowerCase()
+
+   switch(difficulty){
+       case "easy":
+            $('.difficulty').css("color", "green")
+            break;
+
+        case "medium":
+            $('.difficulty').css("color","rgb(255, 174, 0)")
+            break;
+
+        case "hard":
+            $('.difficulty').css("color", 'red');
+            break;
+   }
+}
+
+function boilerPlateCode(){
+    //Boiler Plate Code for autoloading code
+    boilierPlate={
+        //JavaScript
+        "javascript":`function ${functionName}(word){
+                \r}`,
+        
+        //Java
+        "java":`class Program{\r\tpublic static void main (String[]args){
+                        \r\t\tString ${functionName}(String word){
+                            \r\t\t\tSystem.out.println("Hello World!")
+                            \r\t\t}\r\t}\r}`,
+        
+        //Python
+        "python": `def ${functionName}(word):`,
+
+        //C#
+        "csharp": 
+        
+                `public class Program{\r\tpublic static void ${functionName}(){
+                    \r\t\tConsole.WriteLine("Hello World!");
+                    \r\t}
+            \r}`,
+
+        //C++       
+        "c_cpp": 
+                `#include <iostream>\nusing namespace std;\nint ${functionName}(){
+                    \r\tcout << "Hello World" << endl;\r\treturn 0;
+                \r}`,
+        //Clojure
+        "clojure": "IDK"
+
+    }
+
+    return boilierPlate;
+
+}
+
    
-
-
+//Enable Tool Tip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
