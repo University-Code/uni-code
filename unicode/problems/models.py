24266ce226067fcf3a0981bc7e1229548e2dbc5c
John Paul Kiser <johnpaulkiser@gmail.com>
2019-10-09 13:09:48 -0400

    Create ProblemListView

--- unicode/problems/models.py
+++ unicode/problems/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User
 
 ''' Models that represent a unicode problem'''
 
+
 class Problem(models.Model):
     user_submitted = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    example_solution= models.CharField(max_length=1000)
+    example_solution = models.CharField(max_length=1000)
 
-    #make tostring
+    def __str__(self):
+        return self.title
+
 
 class ProblemTestCase(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE, )
@@ -17,10 +20,4 @@ class ProblemTestCase(models.Model):
     test_input = models.CharField(max_length=1000)
     test_output = models.CharField(max_length=1000)
 
-    #make tostring
-    
-
-    
-    
-
-  
+    # make tostring
--- unicode/problems/views.py
+++ unicode/problems/views.py
@@ -1,11 +1,26 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Problem
+
 
 def index(request):
     return render(request, 'problems/index.html', {"title": "Welcome to Uni-code"})
 
+
 def problems_list(request):
     return render(request, 'problems/problems.html', {"title": "Problems"})
 
+
 def create_problem(request):
     return render(request, 'problems/create.html', {"title": "Create Problem"})
+
+
+class ProblemListView(ListView):
+
+    model = Problem
+    paginate_by = 40
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
