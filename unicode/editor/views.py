ab43b8ab4c78c200be09644fe2aede0f50ab351e
JP <johnpaulkiser@gmail.com>
2019-10-09 23:23:58 -0400

    oops

--- unicode/editor/views.py
+++ unicode/editor/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-def editor(request):
-=======
 def playground(request):
->>>>>>> bug-fix
     return render(request, 'editor/playground.html', {"title": "Playground"})
 
 # Create your views here.
