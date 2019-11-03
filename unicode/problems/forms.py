7c0f05bb3d1776fb21130e0bae73141439358a81
John Paul Kiser <johnpaulkiser@gmail.com>
2019-11-03 00:40:47 -0400

    Change description & examplecode to Textarea

--- unicode/problems/forms.py
+++ unicode/problems/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import Problem, ProblemTestCase
 
+ATTRS = {'cols': '40', 'rows': '5'}
 class ProblemForm(forms.ModelForm):
 
     class Meta:
         model = Problem
-        fields = ('title', 'description', 'datatype', 'example_solution')
-
+        fields = ['title', 'datatype', 'description', 'example_solution']
+        widgets = {
+                   'description' : forms.Textarea(attrs=ATTRS),
+                   'example_solution' : forms.Textarea(attrs=ATTRS)
+                  }
 
 #class TestCaseForm(forms.ModelForm):
 #
