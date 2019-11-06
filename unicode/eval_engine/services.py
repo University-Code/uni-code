caf2d0db742a1c13ec451d43259cea4a27147c5a
Andrew <ajbohlman@gmail.com>
2019-11-06 12:32:12 -0500

    Made changes discussed in PR

--- unicode/eval_engine/services.py
+++ unicode/eval_engine/services.py
@@ -1,4 +1,5 @@
 import requests
+from eval_engine.settings import url
 from editor.models import UserSubmission
 from problems.models import Problem
 from problems.models import ProblemTestCase
@@ -19,16 +20,13 @@ lang = {
 
 # Makes the post request to the API and returns an object containing the API output
 def eval_engine(source, lang, test_in, test_out):
-    url = 'http://159.89.93.145:3000/submissions/?base64_encoded=false&wait=true'   # wait=true makes the API call synchronous, halving the number of requests needed
     params = {
         'source_code': source,
         'language_id': lang,
         'stdin': test_in,
         'expected_output': test_out
     }
-    r = requests.post(url, params=params)   # Sends the post request to the api
-    eval_output = r.json()
-    return eval_output
+    return requests.post(url, params=params).json()   # Sends the post request to the api
 
 
 # Function called by view.py in the editor. Submission_id is the primary key of the UserSubmission table
@@ -37,12 +35,12 @@ def eval_setup(submission_id):
     source = sub.submission
     language = sub.language
     # user = sub.submitter
-    test_case = ProblemTestCase.objects.filter(problem = sub.problem)
+    test_cases = ProblemTestCase.objects.filter(problem = sub.problem)
 
-    for case in test_case:
+    for case in test_cases:
         eval_object = eval_engine(source, lang[language], case.test_input, case.test_output)
 
-    if 'Error' in eval_object['status']['description']:
-        print('Compile Failed')
-    else:
-        print(eval_object['status']['description'])
+        if 'Error' in eval_object['status']['description']:
+            print('Compile Failed')
+        else:
+            print(eval_object['status']['description'])
